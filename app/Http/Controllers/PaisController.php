06a8da82c15508e18c97f0014a122f850007e499
Cristian <Cristiannavarromillan@a.cifpperedesongall.org>
2025-01-05 11:53:30 +0100

    Update PaisController.php

--- app/Http/Controllers/PaisController.php
+++ app/Http/Controllers/PaisController.php
@@ -6,59 +6,31 @@ use Illuminate\Http\Request;
 
 class PaisController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         return Pais::all();
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
+        $request->validate(['nombre' => 'required|unique:paises', 'continente' => 'required']);
+        return Pais::create($request->all());
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
+    public function show($id)
     {
-        //
+        return Pais::findOrFail($id);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
+    public function update(Request $request, $id)
     {
-        //
+        $pais = Pais::findOrFail($id);
+        $pais->update($request->all());
+        return $pais;
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
+    public function destroy($id)
     {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        return Pais::destroy($id);
     }
 }
