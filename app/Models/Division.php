d8bee8544f514ffe6144ff859bb76f82fe247aea
Cristian <Cristiannavarromillan@a.cifpperedesongall.org>
2025-01-05 11:43:57 +0100

    Update Division.php

--- app/Models/Division.php
+++ app/Models/Division.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Division extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['nombre', 'region'];
+
+    public function pais()
+    {
+        return $this->belongsTo(Pais::class, 'region');
+    }
 }
