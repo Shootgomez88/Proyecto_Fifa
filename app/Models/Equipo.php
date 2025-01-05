be6f5994b6f59f403b40eb17e69ac968695c3739
Cristian <Cristiannavarromillan@a.cifpperedesongall.org>
2025-01-05 11:43:19 +0100

    Update Equipo.php

--- app/Models/Equipo.php
+++ app/Models/Equipo.php
@@ -2,9 +2,29 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Equipo extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'nombre',
+        'pais',
+        'division',
+        'fundado_en',
+        'estadio',
+        'titulos',
+    ];
+
+    public function pais()
+    {
+        return $this->belongsTo(Pais::class, 'pais');
+    }
+
+    public function division()
+    {
+        return $this->belongsTo(Division::class, 'division');
+    }
 }
