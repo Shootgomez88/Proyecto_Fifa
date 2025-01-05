3eb9e533b896e2c5f46114951aa470a5e7ae135c
Adrian <adriangomezmonje@a.cifpperedesongall.org>
2025-01-05 11:31:03 +0100

    creacion de modelos y controladores

--- app/Http/Controllers/EquipoController.php
+++ app/Http/Controllers/EquipoController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class FifaController extends Controller
+class EquipoController extends Controller
 {
     /**
      * Display a listing of the resource.
