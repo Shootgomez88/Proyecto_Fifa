2f47368096c86927fcb6f8722e2271030e48648e
Cristian <Cristiannavarromillan@a.cifpperedesongall.org>
2025-01-05 11:44:48 +0100

    Update Jugador.php

--- app/Models/Jugador.php
+++ app/Models/Jugador.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Jugador extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['nombre', 'equipo_id', 'pais', 'edad', 'posicion', 'valor_mercado'];
+
+    public function equipo()
+    {
+        return $this->belongsTo(Equipo::class);
+    }
+
+    public function pais()
+    {
+        return $this->belongsTo(Pais::class, 'pais');
+    }
 }
